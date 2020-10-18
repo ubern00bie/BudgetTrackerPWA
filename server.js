c0ea5a5658b2017b1d61126a6dea3aac66455795
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-10-18 11:38:44 -0500

    edits to server for heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+// const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-app.use(compression());
+// app.use(logger("dev"));
+// app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

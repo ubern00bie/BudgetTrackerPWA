e3a6f3aa3e509f8cdbceda87f1ab3a48f32c606f
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-10-17 20:53:09 -0500

    minor updates to manifest & service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,79 +1,73 @@
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_FILES = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/style.css"
+]
 
-// const iconSizes = ["72", "96", "128", "144", "152", "192", "384", "512"];
-// const iconFiles = iconSizes.map(
-//   (size) => `/assets/images/icons/icon-${size}x${size}.png`
-// );
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(CACHE_FILES);
+    })
+  );
 
-// const staticFilesToPreCache = [
-//   "/",
-//   "/app.js",
-//   "/favicon.ico",
-//   "/manifest.webmanifest",
-// ].concat(iconFiles);
+  self.skipWaiting();
+});
 
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 
-// // install
-// self.addEventListener("install", function(evt) {
-//   evt.waitUntil(
-//     caches.open(CACHE_NAME).then(cache => {
-//       console.log("Your files were pre-cached successfully!");
-//       return cache.addAll(staticFilesToPreCache);
-//     })
-//   );
+  self.clients.claim();
+});
 
-//   self.skipWaiting();
-// });
+// fetch
+self.addEventListener("fetch", function(evt) {
+  const {url} = evt.request;
+  if (url.includes("/api")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request, response.clone());
+            }
 
-// // activate
-// self.addEventListener("activate", function(evt) {
-//   evt.waitUntil(
-//     caches.keys().then(keyList => {
-//       return Promise.all(
-//         keyList.map(key => {
-//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-//             console.log("Removing old cache data", key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
-
-//   self.clients.claim();
-// });
-
-// // fetch
-// self.addEventListener("fetch", function(evt) {
-//   const {url} = evt.request;
-//   if (url.includes("/all") || url.includes("/find")) {
-//     evt.respondWith(
-//       caches.open(DATA_CACHE_NAME).then(cache => {
-//         return fetch(evt.request)
-//           .then(response => {
-//             // If the response was good, clone it and store it in the cache.
-//             if (response.status === 200) {
-//               cache.put(evt.request, response.clone());
-//             }
-
-//             return response;
-//           })
-//           .catch(err => {
-//             // Network request failed, try to get it from the cache.
-//             return cache.match(evt.request);
-//           });
-//       }).catch(err => console.log(err))
-//     );
-//   } else {
-//     // respond from static cache, request is not for /api/*
-//     evt.respondWith(
-//       caches.open(CACHE_NAME).then(cache => {
-//         return cache.match(evt.request).then(response => {
-//           return response || fetch(evt.request);
-//         });
-//       })
-//     );
-//   }
-// });
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+  } else {
+    // respond from static cache, request is not for /api/*
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  }
+});
